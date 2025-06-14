99038a74322c2dec0d4eaff3dbca154315efcf54
Marco Aurelio <teoaa2@gmail.com>
2025-06-14 18:01:57 -0300

    feat: criar PessoaMapper

--- pessoa-service/src/main/java/com/trepudox/pessoaservice/infra/mapper/PessoaMapper.java
+++ pessoa-service/src/main/java/com/trepudox/pessoaservice/infra/mapper/PessoaMapper.java
@@ -1,12 +1,21 @@
 package com.trepudox.pessoaservice.infra.mapper;
 
+import com.trepudox.pessoaservice.core.dto.PessoaDTO;
+import com.trepudox.pessoaservice.infra.persistence.model.PessoaModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper(imports = {LocalDateTime.class})
 public interface PessoaMapper {
 
-    // TODO
+    PessoaMapper INSTANCE = Mappers.getMapper(PessoaMapper.class);
 
+    PessoaModel pessoaDTOToPessoaModel(PessoaDTO pessoaDTO);
+
+    PessoaDTO pessoaModelToPessoaDTO(PessoaModel pessoaModel);
+
+    List<PessoaDTO> pessoaModelListToPessoaDTOList(List<PessoaModel> pessoaModelList);
 }
