f744a8a2a8f8bf59deb90787a2fe17c5a2c938c0
Marco Aurelio <teoaa2@gmail.com>
2025-06-14 18:00:36 -0300

    feat: desenvolver PessoaRepositoryAdapter

--- pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/out/PessoaRepositoryAdapater.java
+++ pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/out/PessoaRepositoryAdapater.java
@@ -3,6 +3,8 @@ package com.trepudox.pessoaservice.adapter.out;
 import com.trepudox.pessoaservice.core.dto.PessoaDTO;
 import com.trepudox.pessoaservice.core.port.out.DatabaseOutputPort;
 
+import com.trepudox.pessoaservice.infra.mapper.PessoaMapper;
+import com.trepudox.pessoaservice.infra.persistence.model.PessoaModel;
 import com.trepudox.pessoaservice.infra.persistence.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,14 +19,32 @@ public class PessoaRepositoryAdapater implements DatabaseOutputPort {
     private final PessoaRepository pessoaRepository;
 
     @Override
+    public PessoaDTO save(PessoaDTO pessoaDTO) {
+        PessoaModel pessoaModel = PessoaMapper.INSTANCE.pessoaDTOToPessoaModel(pessoaDTO);
+        pessoaModel = pessoaRepository.save(pessoaModel);
+
+        return PessoaMapper.INSTANCE.pessoaModelToPessoaDTO(pessoaModel);
+    }
+
+    @Override
     public List<PessoaDTO> getAll() {
-        // TODO
-        return List.of();
+        return PessoaMapper.INSTANCE.pessoaModelListToPessoaDTOList(pessoaRepository.findAll());
     }
 
     @Override
     public Optional<PessoaDTO> getById(Long id) {
-        // TODO
+        Optional<PessoaModel> pessoaModelOptional = pessoaRepository.findById(id);
+
+        if (pessoaModelOptional.isPresent()) {
+            PessoaDTO pessoaDTO = PessoaMapper.INSTANCE.pessoaModelToPessoaDTO(pessoaModelOptional.get());
+            return Optional.of(pessoaDTO);
+        }
+
         return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        pessoaRepository.deleteById(id);
     }
 }
