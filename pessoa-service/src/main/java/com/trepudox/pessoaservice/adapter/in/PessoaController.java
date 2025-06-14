86ac1ed70d447be93053514ef7d2a145f49d31b9
Marco Aurelio <teoaa2@gmail.com>
2025-06-14 18:00:04 -0300

    feat: desenvolver PessoaController

--- pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/in/PessoaController.java
+++ pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/in/PessoaController.java
@@ -1,25 +1,35 @@
 package com.trepudox.pessoaservice.adapter.in;
 
+import com.trepudox.pessoaservice.core.dto.PessoaDTO;
+import com.trepudox.pessoaservice.core.port.in.PessoaInputPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/pessoa")
+@RequiredArgsConstructor
 public class PessoaController {
 
+    private final PessoaInputPort pessoaInputPort;
+
     @GetMapping("/")
-    public ResponseEntity<?> getAll() {
-        // TODO
-        return ResponseEntity.status(200).build();
+    public ResponseEntity<List<PessoaDTO>> getAll() {
+        List<PessoaDTO> pessoaDTOList = pessoaInputPort.getAll();
+
+        return ResponseEntity.status(200).body(pessoaDTOList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id) {
-        // TODO
-        return ResponseEntity.status(200).build();
+    public ResponseEntity<PessoaDTO> getById(@PathVariable Long id) {
+        PessoaDTO pessoaDTO = pessoaInputPort.getById(id);
+
+        return ResponseEntity.status(200).body(pessoaDTO);
     }
 
 }
