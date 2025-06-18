e66f86f65ab31545eec6b8cb746c5cb314fe2f1d
Marco Aurelio <teoaa2@gmail.com>
2025-06-18 01:03:47 -0300

    feat: iniciando acknowledge manual das mensagens

--- cargo-service/src/main/java/com/trepudox/cargoservice/adapter/in/CargoRabbitConsumer.java
+++ cargo-service/src/main/java/com/trepudox/cargoservice/adapter/in/CargoRabbitConsumer.java
@@ -1,13 +1,17 @@
 package com.trepudox.cargoservice.adapter.in;
 
+import com.rabbitmq.client.Channel;
 import com.trepudox.cargoservice.core.dto.CargoDTO;
 import com.trepudox.cargoservice.core.port.in.CargoInputPort;
 import com.trepudox.cargoservice.infra.mapper.JsonMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -17,19 +21,35 @@ public class CargoRabbitConsumer {
     private final JsonMapper jsonMapper;
     private final CargoInputPort cargoInputPort;
 
-    // TODO: tratamento de exceção jogando na DLQ, criar um metodo separado pro case default do switch case
+    // TODO: tratamento de exceção jogando na DLQ
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
     public void receiveMessage(String messageContent,
-                        @Header(name = "action") String actionHeader) {
-        log.info("ActionHeader: {} - Message: {}", actionHeader, messageContent);
+                               @Header(name = "action") String actionHeader,
+                               @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag,
+                               Channel channel) throws IOException {
+        log.info("Delivery Tag : {} - ActionHeader: {} - Message: {}", deliveryTag, actionHeader, messageContent);
 
-        CargoDTO cargoDTO = jsonMapper.fromJson(messageContent, CargoDTO.class);
+        try {
+            CargoDTO cargoDTO = jsonMapper.fromJson(messageContent, CargoDTO.class);
+            processMessage(actionHeader, cargoDTO);
+            channel.basicAck(deliveryTag, false);
 
-        switch (actionHeader.toLowerCase()) {
+            log.info("Message with delivery tag '{}' was processed and acknowledged successfully", deliveryTag);
+        } catch (Exception e) {
+            log.error("It wasn't possible to process this message - Delivery tag: {} - Error: {}", deliveryTag, e.toString());
+            channel.basicReject(deliveryTag, false);
+            log.warn("The message was rejected");
+        }
+
+
+    }
+
+    private void processMessage(String action, CargoDTO cargoDTO) {
+        switch (action.toLowerCase()) {
             case "create" -> cargoInputPort.create(cargoDTO);
             case "update" -> cargoInputPort.update(cargoDTO);
             case "delete" -> cargoInputPort.deleteById(cargoDTO.getId());
-            default -> throw new IllegalArgumentException("No action defined for '%s'".formatted(actionHeader));
+            default -> throw new IllegalArgumentException("No action defined for '%s'".formatted(action));
         }
     }
 
