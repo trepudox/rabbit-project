adbe9d52ff88b95e7068c0b11ff02e7ec433f870
Marco Aurelio <teoaa2@gmail.com>
2025-06-26 10:34:23 -0300

    feat: configurar producer da DLQ, usa-lo no consumer e adicionar propriedades necessarias no application.yml

--- cargo-service/src/main/java/com/trepudox/cargoservice/adapter/in/CargoRabbitConsumer.java
+++ cargo-service/src/main/java/com/trepudox/cargoservice/adapter/in/CargoRabbitConsumer.java
@@ -1,14 +1,14 @@
 package com.trepudox.cargoservice.adapter.in;
 
 import com.rabbitmq.client.Channel;
+import com.trepudox.cargoservice.adapter.out.CargoRabbitDlqProducer;
 import com.trepudox.cargoservice.core.dto.CargoDTO;
 import com.trepudox.cargoservice.core.port.in.CargoInputPort;
 import com.trepudox.cargoservice.infra.mapper.JsonMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -20,13 +20,14 @@ public class CargoRabbitConsumer {
 
     private final JsonMapper jsonMapper;
     private final CargoInputPort cargoInputPort;
+    private final CargoRabbitDlqProducer cargoRabbitDlqProducer;
 
-    // TODO: tratamento de exceção jogando na DLQ
-    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
-    public void receiveMessage(String messageContent,
-                               @Header(name = "action") String actionHeader,
-                               @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag,
-                               Channel channel) throws IOException {
+    @RabbitListener(queues = {"${rabbitmq.consumer.queue.name}"})
+    public void receiveMessage(Message message, Channel channel) throws IOException {
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        String actionHeader = message.getMessageProperties().getHeader("action");
+        String messageContent = new String(message.getBody());
+
         log.info("Delivery Tag : {} - ActionHeader: {} - Message: {}", deliveryTag, actionHeader, messageContent);
 
         try {
@@ -37,7 +38,7 @@ public class CargoRabbitConsumer {
             log.info("Message with delivery tag '{}' was processed and acknowledged successfully", deliveryTag);
         } catch (Exception e) {
             log.error("It wasn't possible to process this message - Delivery tag: {} - Error: {}", deliveryTag, e.toString());
-            channel.basicReject(deliveryTag, false);
+            sendMessageToDLQ(message, channel, deliveryTag);
             log.warn("The message was rejected");
         }
 
@@ -49,6 +50,17 @@ public class CargoRabbitConsumer {
             case "update" -> cargoInputPort.update(cargoDTO);
             case "delete" -> cargoInputPort.deleteById(cargoDTO.getId());
             default -> throw new IllegalArgumentException("No action defined for '%s'".formatted(action));
+        }
+    }
+
+    private void sendMessageToDLQ(Message message, Channel channel, long deliveryTag) throws IOException {
+        try {
+            cargoRabbitDlqProducer.sendMessage(message);
+            log.info("The message was sent to the DLQ.");
+            channel.basicReject(deliveryTag, false);
+        } catch(Exception e) {
+            log.info("It wasn't possible to send this message to the DLQ. Requeuing...");
+            channel.basicReject(deliveryTag, true);
         }
     }
 
--- cargo-service/src/main/java/com/trepudox/cargoservice/adapter/out/CargoRabbitDlqProducer.java
+++ cargo-service/src/main/java/com/trepudox/cargoservice/adapter/out/CargoRabbitDlqProducer.java
@@ -2,6 +2,9 @@ package com.trepudox.cargoservice.adapter.out;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,6 +12,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CargoRabbitDlqProducer {
 
-    // TODO
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.producer.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.producer.routing-key}")
+    private String routingKey;
+
+    public void sendMessage(Message message) {
+        log.info("Sending message to the exchange '{}' with following routing key {}. Message: {}", exchange, routingKey, new String(message.getBody()));
+        rabbitTemplate.send(exchange, routingKey, message);
+        log.info("Message sent successfully.");
+
+    }
 
 }
