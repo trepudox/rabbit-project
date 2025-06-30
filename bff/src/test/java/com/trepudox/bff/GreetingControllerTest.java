458c1eb4a7b607cb7796f3dcee5f4a2b367534c3
Marco Aurelio <teoaa2@gmail.com>
2025-06-30 19:41:41 -0300

    feat: mudar stack do bff de spring pra quarkus

---
+++ bff/src/test/java/com/trepudox/bff/GreetingControllerTest.java
@@ -0,0 +1,22 @@
+package com.trepudox.bff;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class GreetingControllerTest {
+
+    @Test
+    void testHelloEndpoint() {
+        given().contentType(ContentType.JSON)
+          .when().get("/hello/again")
+          .then()
+             .statusCode(200)
+             .body(is("{\"message\": \"Hello World!\"}"));
+    }
+
+}
