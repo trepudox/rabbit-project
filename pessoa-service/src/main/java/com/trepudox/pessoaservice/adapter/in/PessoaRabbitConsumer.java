3b82010f034b96143ce79de036643eb08f7cd4af
Marco Aurelio <teoaa2@gmail.com>
2025-07-11 18:48:57 -0300

    estrutura inicial

--- pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/in/PessoaRabbitConsumer.java
+++ pessoa-service/src/main/java/com/trepudox/pessoaservice/adapter/in/PessoaRabbitConsumer.java
@@ -1,7 +1,19 @@
 package com.trepudox.pessoaservice.adapter.in;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class PessoaRabbitConsumer {
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void receive(String messageContent,
+                        @Header(name = "action") String actionHeader) {
+        log.info("Message {}", messageContent);
+        log.info("ActionHeader {}", actionHeader);
+    }
+
 }
